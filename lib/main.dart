8eb2c310c0bc101760aafcbbc0879cedff65e17d
Saúl Calderón <saulecc@gmail.com>
2026-07-30 09:31:53 -0600

    S05 estructura mínima de Inkash

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Inkash',
+      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
+      home: Scaffold(
+        body: SafeArea(
+          child: ListView(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [Text('Hola, Kevin'), Text('Julio 2026')],
+              ),
+              Column(
+                children: [
+                  Text(
+                    'TE QUEDAN DISPONIBLES',
+                    style: TextStyle(fontSize: 100),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+// const SizedBox(height: 22),
+// Column(
+//   crossAxisAlignment: CrossAxisAlignment.start,
+//   children: const [
+//     Text('TE QUEDAN DISPONIBLES', style: TextStyle(fontSize: 11)),
+//     SizedBox(height: 6),
+//     Text('Q2,796.50', style: TextStyle(fontSize: 52)),
+//     SizedBox(height: 16),
+//     LinearProgressIndicator(value: 0.57),
+//     SizedBox(height: 7),
+//     Text('Has usado Q3,703.50 de Q6,500.00', style: TextStyle(fontSize: 12)),
+//   ],
+// ),
