2b38e8bbdf85921f35e9b71e397f4caf5ee1af78
Saúl Calderón <saulecc@gmail.com>
2026-07-30 09:51:42 -0600

    S05 estructura inicial top widgets

--- lib/main.dart
+++ lib/main.dart
@@ -16,16 +16,25 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         body: SafeArea(
           child: ListView(
+            padding: EdgeInsets.all(16.0),
             children: [
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [Text('Hola, Kevin'), Text('Julio 2026')],
               ),
+              const SizedBox(height: 22),
               Column(
-                children: [
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const [
+                  Text('TE QUEDAN DISPONIBLES', style: TextStyle(fontSize: 11)),
+                  SizedBox(height: 6),
+                  Text('Q2,796.50', style: TextStyle(fontSize: 52)),
+                  SizedBox(height: 16),
+                  LinearProgressIndicator(value: 0.57),
+                  SizedBox(height: 7),
                   Text(
-                    'TE QUEDAN DISPONIBLES',
-                    style: TextStyle(fontSize: 100),
+                    'Has usado Q3,703.50 de Q6,500.00',
+                    style: TextStyle(fontSize: 12),
                   ),
                 ],
               ),
@@ -36,17 +45,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-// const SizedBox(height: 22),
-// Column(
-//   crossAxisAlignment: CrossAxisAlignment.start,
-//   children: const [
-//     Text('TE QUEDAN DISPONIBLES', style: TextStyle(fontSize: 11)),
-//     SizedBox(height: 6),
-//     Text('Q2,796.50', style: TextStyle(fontSize: 52)),
-//     SizedBox(height: 16),
-//     LinearProgressIndicator(value: 0.57),
-//     SizedBox(height: 7),
-//     Text('Has usado Q3,703.50 de Q6,500.00', style: TextStyle(fontSize: 12)),
-//   ],
-// ),
